e89c08e27bc0a4d211cc855fc90d459569e763b5
Platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-14 20:24:06 +0200

    Try more functions

--- OpenCV_Project/Chapter3.cpp
+++ OpenCV_Project/Chapter3.cpp
@@ -8,17 +8,32 @@
 using namespace std;
 using namespace cv;
 
+//Just trying what diff functions do
+void blurEdgeHighlight(Mat img, int Strength) {
+	Mat final_img;
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(7, 7));
+	GaussianBlur(img, final_img, Size(1, 1), 3, 0);
+	dilate(final_img, final_img, kernel);
+	imshow("PortraitMode", final_img);
+}
+
+
 int main() {
 	string path = "Images\\img1.jpg";
-	Mat img, img_resized, img_blur, img_canny;
+	Mat img, img_resized, img_blur, img_canny, img_dilated, kernel;
 	img = imread(path);
 	//Test basic functions
 	resize(img, img_resized, Size(500, 500));
 	GaussianBlur(img_resized, img_blur, Size(7, 7), 5, 0);
 	Canny(img_blur, img_canny, 25, 80);
+	//dilate(img_canny, img_dilated, kernel);
+
+	//Test portrait
+	blurEdgeHighlight(img_resized, 5);
 	//Show the results
-	imshow("Test", img_blur);
+	/*imshow("Test", img_blur);
 	imshow("Canny", img_canny);
+	imshow("Dilated", img_dilated);*/
 	waitKey(0);
 	return 0;
 
