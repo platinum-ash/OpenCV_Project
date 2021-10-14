ad9c437e5dc6081c5daae902c656ac64b3314859
Platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-14 19:29:31 +0200

    Add project files.

---
+++ OpenCV_Project/Chapter3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+
+
+
+}
