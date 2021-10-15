ebd82ca4c20e342116a65215044122d92276a304
platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-15 23:06:25 +0200

    Cleanup and test crop

--- OpenCV_Project/Chapter3.cpp
+++ OpenCV_Project/Chapter3.cpp
@@ -19,23 +19,27 @@ Mat blurEdgeHighlight(Mat img, int Strength) {
 
 
 int main() {
-	string path = "Images\\img1.jpg";
-	Mat img, img_resized, img_blur, img_canny, img_dilated, kernel;
+	string path = "Images//img1.jpg";
+	Mat img, img_resized, img_blur, img_canny, img_dilated, kernel, img_crop;
 	img = imread(path);
 	//Test basic functions
 	resize(img, img_resized, Size(500, 500));
 	GaussianBlur(img_resized, img_blur, Size(7, 7), 5, 0);
 	Canny(img_blur, img_canny, 25, 80);
-	//dilate(img_canny, img_dilated, kernel);
-
-	//Test portrait
+	dilate(img_canny, img_dilated, kernel);
+	//Test image crop functions
+	Rect crop_dimension(200, 200, 300, 250);
+	img_crop = img_resized(crop_dimension);
+	//Test blurEdge
 	imshow("Image from function", blurEdgeHighlight(img_resized, 5));
-	//Show the results
-	/*imshow("Test", img_blur);
+	//Test crop
+	imshow("Cropped image", img_crop);
+	//Test Canny
 	imshow("Canny", img_canny);
-	imshow("Dilated", img_dilated);*/
+	//Test dilation
+	imshow("Dilated image", img_dilated);
+
+	
 	waitKey(0);
 	return 0;
-
-
 }
