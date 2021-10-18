9d8a951768c5f9a69adef013875c43811ac773f2
Platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-19 01:27:06 +0200

    Fix saturation trackbar crashes

--- OpenCV_Project/Source.cpp
+++ OpenCV_Project/Source.cpp
@@ -13,13 +13,12 @@ int main() {
 
 	int h_min = 0, s_min = 0, v_min = 0;
 	int h_max = 179, s_max = 255, v_max = 255;
-
 	namedWindow("Trackbar", (650, 800));
 	createTrackbar("Hue lower", "Trackbar", &h_min, 179);
 	createTrackbar("Saturation lower", "Trackbar", &s_min, 255);
 	createTrackbar("Value lower", "Trackbar", &v_min, 255);
 	createTrackbar("Hue upper", "Trackbar", &h_max, 179);
-	createTrackbar("Saturation lower", "Trackbar", &s_max, 255);
+	createTrackbar("Saturation upper", "Trackbar", &s_max, 255);
 	createTrackbar("Value max", "Trackbar", &v_max, 255);
 
 	Mat img = imread("Images//img5.jpg");
@@ -30,7 +29,6 @@ int main() {
 
 		Scalar lower_b(h_min, s_min, v_min);
 		Scalar upper_b(h_max, s_max, v_max);
-		
 		imshow("Original", img);
 		
 		inRange(img_new_colors, lower_b, upper_b, mask);
