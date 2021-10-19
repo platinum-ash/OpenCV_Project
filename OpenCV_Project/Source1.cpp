315626496dc1205cb04fa520846eb7dcedc451ef
Platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-19 13:16:41 +0200

    Add size check function. Add contour feature with noise detection

--- OpenCV_Project/Source1.cpp
+++ OpenCV_Project/Source1.cpp
@@ -13,8 +13,21 @@ using namespace cv;
 void getContours(Mat img_dil, Mat img){
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
-	findContours(img_dil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE),
-	drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+	findContours(img_dil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(0, 0, 0), 8);
+	double area = 0;
+	for (int i = 0; i < contours.size(); i++) {
+		area = contourArea(contours[i]);
+
+		if (area > 2500) {
+			drawContours(img, contours, i, Scalar(0, 0, 0), 8);
+		}
+		//cout << area << endl;
+	}
+
+
+
+	imshow("Dilated image passed to the function", img_dil);
 	imshow("Contoured image", img);
 	waitKey(0);
 
@@ -35,11 +48,30 @@ Mat preProcessor(Mat img) {
 }
 
 
+//Check size of image and shrink it if necessary
+void checkSize(Mat &img) {
+	Size img_size = img.size();
+	int width = img_size.width;
+	int height = img_size.height;
+	if (height > 1000 || width > 1000) {
+		resize(img, img, Size(800, 600));
+	}
+}
 
 int main() {
 	Mat img;
-	string path = "Images//img6.png";
-	img = imread(path);
+	//For now use console input for the image file path.
+	//TODO : ACCEPTH IMAGE PATH AS COMMAND LINE INPUT WHEN THE PROGRAM STARTS
+	string userInput;
+	cout << "Please input path of the image: ";
+	getline(cin, userInput);
+
+	cout << "The string that was read was :  " << userInput << endl;
+	//Check if image needs to be shrinked
+	//string path = "Images//img6.png";
+	img = imread(userInput);
+	checkSize(img);
+
 	Mat img_contour = preProcessor(img);
 	getContours(img_contour, img);
 
