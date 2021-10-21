12d673bffbfed25d02abf593a630eb2778e3de24
Platinum <87782883+platinum-ash@users.noreply.github.com>
2021-10-21 20:41:23 +0200

    remove deleted function from declaration

--- OpenCV_Project/image_processing.h
+++ OpenCV_Project/image_processing.h
@@ -6,9 +6,8 @@
 #include "image_processing.cpp"
 
 void findHSV(Mat &img);
-void getContours(Mat &img_dil, Mat &img);
 Mat preProcessor(Mat &img);
 void shapeDetect(Mat &img, Mat &img_dil);
-vector<vector<Point>> findContours(Mat &img_dil);
+vector<vector<Point>> findContoursLocal(Mat &img_dil);
 void drawContoursLocal(Mat &img, vector<vector<Point>> &contours);
 #endif // !IMAGE_PROCESSING_H
